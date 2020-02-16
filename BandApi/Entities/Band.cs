eb8e55ab926845f8cf16dbe73ccb6ef9a167d0cf
Charles Burch <georgia_beast89@yahoo.com>
2020-02-16 18:24:41 -0500

    add script to seed the ablum and band data

--- BandApi/Entities/Band.cs
+++ BandApi/Entities/Band.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace BandApi.Entities
 {
     public class Band
     {
-        public Band()
-        {
-        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public DateTime Founded { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string MainGenre { get; set; }
+
+        public ICollection<Album> Albums { get; set; } = new List<Album>();
     }
 }
