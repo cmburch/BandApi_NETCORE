686e7843e49364a41f08264bb61ade6b5d874da2
Charles Burch <georgia_beast89@yahoo.com>
2020-02-16 17:22:40 -0500

    added the BandAlbumContext

--- BandApi/DbContexts/BandAlbumContext.cs
+++ BandApi/DbContexts/BandAlbumContext.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using BandApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace BandApi.DbContexts
 {
-    public class BandAlbumContext
+
+public class BandAlbumContext : DbContext
     {
-        public BandAlbumContext()
+        public BandAlbumContext(DbContextOptions<BandAlbumContext> options):
+            base(options)
         {
         }
+
+        public DbSet<Band> Bands { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
     }
 }
