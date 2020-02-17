96d86f1f0bdbdaa8bacb1b77f22bffb1babe470f
Charles Burch <georgia_beast89@yahoo.com>
2020-02-16 23:40:26 -0500

    add interface for band and album repository

---
+++ BandApi/Services/IBandAlbumRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using BandApi.Entities;
+
+namespace BandApi.Services
+{
+    public interface IBandAlbumRepository
+    {
+        //ALBUMS
+        IEnumerable<Album> GetAlbums(Guid bandId);
+
+        Album GetAlbum(Guid bandId, Guid albumId);
+
+        void AddAlbum(Guid bandId, Album album);
+
+        void UpdateAlbum(Album album);
+
+        void DeleteAlbum(Album album);
+
+
+        //BANDS
+        IEnumerable<Band> GetBands();
+
+        Band GetBand(Guid bandId);
+
+        IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds);
+
+        void AddBand(Band band);
+
+        void UpdateBand(Band band);
+
+        void DeleteBand(Band band);
+
+
+
+        bool BandExists(Guid bandId);
+
+        bool AlbumExists(Guid albumId);
+
+        bool Save();
+
+
+    }
+}
