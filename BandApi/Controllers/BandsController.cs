68d70d59749a667c609d7c6d53aa285e8cfae8fd
Charles Burch <georgia_beast89@yahoo.com>
2020-02-18 12:40:28 -0500

    created controller for bands and add the contoller for getting all bands

---
+++ BandApi/Controllers/BandsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BandApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandApi.Controllers
+{
+    [ApiController]
+    [Route("api/bands")]
+    public class BandsController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandsController(IBandAlbumRepository bandAlbumRepository)
+        {
+            _bandAlbumRepository = bandAlbumRepository ??
+                throw new ArgumentNullException(nameof(bandAlbumRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetBands()
+        {
+            var bandsFromRepo = _bandAlbumRepository.GetBands();
+            return new JsonResult(bandsFromRepo);
+        }
+    }
+}
