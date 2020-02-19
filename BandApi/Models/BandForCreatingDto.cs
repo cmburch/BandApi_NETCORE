e305a522b9781009f9c71833084ea63bdd5ac055
Charles Burch <georgia_beast89@yahoo.com>
2020-02-19 06:24:00 -0500

    added dto for creating bands

---
+++ BandApi/Models/BandForCreatingDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BandApi.Models
+{
+    public class BandForCreatingDto
+    {
+        public string Name { get; set; }
+
+        public DateTime Founded { get; set; }
+
+        public string MainGenre { get; set; }
+    }
+}
