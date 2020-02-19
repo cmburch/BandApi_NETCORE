756c4a49d45b76f453646bfdd557ccef81decb0b
Charles Burch <georgia_beast89@yahoo.com>
2020-02-18 22:21:29 -0500

    added the albums controller

---
+++ BandApi/Controllers/AlbumsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMapper;
+using BandApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandApi.Controllers
+{
+    [ApiController]
+    [Route("api/bands/{bandId}/albums")]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+        private readonly IMapper _mapper;
+
+        public AlbumsController(IBandAlbumRepository bandAlbumRepository, IMapper mapper)
+        {
+            _bandAlbumRepository = bandAlbumRepository ??
+                throw new ArgumentNullException(nameof(bandAlbumRepository));
+
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
