7104cb42008aef1a55205d127d28a3abdf688146
Charles Burch <georgia_beast89@yahoo.com>
2020-02-18 22:39:02 -0500

    added albumsDto

---
+++ BandApi/Models/AlbumsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BandApi.Models
+{
+    public class AlbumsDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid BandId { get; set; }
+    }
+}
---
+++ BandApi/Profiles/AlbumsProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+
+
+namespace BandApi.Profiles
+{
+    public class AlbumsProfile : Profile
+    {
+        public AlbumsProfile()
+        {
+            CreateMap<Entities.Album, Models.AlbumsDto>().ReverseMap();
+        }
+    }
+}
+
