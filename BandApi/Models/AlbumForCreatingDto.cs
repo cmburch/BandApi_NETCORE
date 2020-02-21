cb883a2a1579801a6848b6932c41a58151a45bf4
Charles Burch <georgia_beast89@yahoo.com>
2020-02-20 21:16:27 -0500

    added validation with Ivalidator Object to the albums to check for same title and description

--- BandApi/Models/AlbumForCreatingDto.cs
+++ BandApi/Models/AlbumForCreatingDto.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BandApi.Models
 {
-    public class AlbumForCreatingDto
+    public class AlbumForCreatingDto : IValidatableObject
     {
         [Required]
         [MaxLength(200)]
@@ -11,5 +12,13 @@ namespace BandApi.Models
 
         [MaxLength(400)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult("Title Must Be Different From Description", new[] { "AlbumForCreatingDto" });
+            }
+        }
     }
 }
